b354e57d2aa22e98dc918cf093d5cdd9a4e8a7c4
Avery <caverym@outlook.com>
2019-10-26 09:39:24 -0400

    cmon

--- com/form-submission-handler.js
+++ com/form-submission-handler.js
@@ -67,7 +67,7 @@
     var xhr = new XMLHttpRequest();
     xhr.open('POST', url);
     // xhr.withCredentials = true;
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded", "Access-Control-Allow-Origin:*");
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
           form.reset();
