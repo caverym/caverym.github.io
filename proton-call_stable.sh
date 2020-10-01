f6bc649d8c7dea2444b94bdc557d261e2bf4aeb7
Avery Murray <caverym@outlook.com>
2020-10-01 17:33:36 -0400

    Update proton-call_stable.sh

--- proton-call_stable.sh
+++ proton-call_stable.sh
@@ -1,13 +1,9 @@
-pkgname=Proton-Caller
-pkgver=0.5.2
-pkgdest=/usr/bin/
-
-mkdir ~/.cache/$pkgname
-cd ~/.cache/$pkgname
-wget https://github.com/caverym/$pkgname/archive/$pkgver.tar.gz
-tar -xf $pkgver.tar.gz
+mkdir ~/.cache/Proton-Caller
+cd ~/.cache/Proton-Caller
+wget https://github.com/caverym/Proton-Caller/archive/0.5.2.tar.gz
+tar -xf $0.5.2.tar.gz
 sudo -v
-cd $pkgname-$pkgver
+cd Proton-Caller-0.5.2
 sudo sh ./install.sh
 cd
-sudo rm -rf ~/.cache/$pkgname
+sudo rm -rf ~/.cache/Proton-Caller
