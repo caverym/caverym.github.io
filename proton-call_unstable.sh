030525870706debf6f4f6ea76b621fc548ee2f2b
Avery Murray <caverym@outlook.com>
2020-10-01 17:12:16 -0400

    Update proton-call_unstable.sh

--- proton-call_unstable.sh
+++ proton-call_unstable.sh
@@ -2,8 +2,7 @@ pkgname=Proton-Caller
 pkgver=alpha
 pkgdest=/usr/bin/
 
-sudo -v
-mkdir ~/.cache/$pkgname
+mkdir -p ~/.cache/$pkgname
 cd ~/.cache/$pkgname
 wget https://github.com/caverym/Proton-Caller/archive/master.zip
 unzip -o master.zip
@@ -11,4 +10,4 @@ sudo -v
 cd $pkgname-master
 sudo sh ./install.sh
 cd
-rm -rf ~/.cache/$pkgname
+sudo rm -rf ~/.cache/$pkgname
