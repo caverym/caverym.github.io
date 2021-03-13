7f86bcbc21ec28a3f2b82e445d27e8d1f0e6c37b
caverym <averylapine@gmail.com>
2021-03-13 16:16:48 -0500

    I did a thing
    Signed-off-by: caverym <averylapine@gmail.com>

--- proton-call_stable.sh
+++
@@ -1,9 +0,0 @@
-mkdir ~/.cache/Proton-Caller
-cd ~/.cache/Proton-Caller
-wget https://github.com/caverym/Proton-Caller/archive/0.5.4.tar.gz
-tar -xf $0.5.4.tar.gz
-sudo -v
-cd Proton-Caller-0.5.4
-sudo sh ./install.sh
-cd
-sudo rm -rf ~/.cache/Proton-Caller
